3d21c2d24fd0fe58cb265d1ab7616d166f30c70b
Mohamed Faris <mohd.faris07@gmail.com>
2023-02-11 11:34:51 +0300

    added moment and trying to parse dates properly

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Box from './Box'
+import moment from 'moment'
 
 export default function Day(props) {
   let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday']
+  let date = moment().add(props.number - 1, 'days').format('DD MMM')
   return (
     <>
       <div className='w-15 text-left self-center w-full'>
         <div className='text-center border-y-2 border-x'>
+          {date}
           {days[props.number - 1]}
         </div>
         <div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-    <div className='bg-green-700 w-screen p-5 text-white flex justify-between mb-3'>
-        <div className='font-extrabold text-2xl'>Foundation Year Calendar</div>
-        <div>Login</div>
+      <div className='bg-green-700 w-screen p-5 text-white flex justify-between mb-3'>
+        <div className='font-extrabold text-2xl'><a href='/'>Foundation Year Calendar</a></div>
+        <div><a href='/login'>Login</a></div>
       </div>
     </>
   )
