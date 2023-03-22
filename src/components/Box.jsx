57a6babc85d0d6757bed7d1bcc4d0e217f176a2b
Mohamed Faris <mohd.faris07@gmail.com>
2023-03-22 19:15:30 +0300

    added flex to Box to fix overflow issue

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -5,7 +5,7 @@ export default function Box(props) {
     <div key={index} className='border border-gray-100 p-2'>{element}</div>
     ))
   return (
-    <div className='w-15 h-32 text-center border border-gray-100 p-5'>
+    <div className='w-15 h-auto text-center border border-gray-100 p-5 flex flex-col'>
       {elements}
     </div>
   )
