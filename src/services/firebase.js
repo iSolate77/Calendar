479a9550ebeb3d2aac28ee2ee33d6b2f38c7d779
Mohamed Faris <mohd.faris07@gmail.com>
2023-04-07 12:20:35 +0300

    configure env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
 import { getDatabase, ref, get} from 'firebase/database'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,20 +7,19 @@ import { getDatabase, ref, get} from 'firebase/database'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:process.env.API_KEY,
-  authDomain: `${process.env.PROJECT_ID}.firebaseapp.com`,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
   databaseURL:
-    `https://${process.env.PROJECT_ID}-default-rtdb.europe-west1.firebasedatabase.app`,
-  projectId: `${process.env.PROJECT_ID}`,
-  storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId:process.env.APP_ID,
-  measurementId:process.env.MEASUREMENT_ID,
+    `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}-default-rtdb.europe-west1.firebasedatabase.app`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSIGING_SENDER_ID,
+  appId:process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId:process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
-export const analytics = getAnalytics(app)
 
 export const db = getDatabase()
 
